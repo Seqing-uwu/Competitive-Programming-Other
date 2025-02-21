b63712bf3321560a81564d9f047d2c74df41ba34
Seqing <rushiatable@gmail.com>
2025-02-21 12:02:42 +0800

    Create joinstrings (editting) .cpp

---
+++ Kattis /joinstrings (editting) .cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+const int N = 169420;
+const int HEAD = 0, END = -1;
+
+string s[N];
+int list[N];
+
+void init() {
+	
+}
+
+void process() {
+	int a, b;
+	while (cin >> a >> b) {
+		
+	}
+	
+} 3-2-4-1
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n; cin >> n;
+	
+	for (int i=1; i<=n; i++) {
+		cin >> s[i];
+	}
+	
+	init();
+	process();
+	
+	return 0;
+}
