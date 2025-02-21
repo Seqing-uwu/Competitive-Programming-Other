c9b82982b44cb898917b1bb955db25331aac76f5
Seqing <rushiatable@gmail.com>
2025-02-21 22:15:29 +0800

    Update a036 白癡題目不要寫.cpp

--- zerojudge/a036 白癡題目不要寫.cpp
+++ zerojudge/a036 白癡題目不要寫.cpp
@@ -2,32 +2,26 @@
 using namespace std;
 
 long long C(int n, int r) {
-	long long a = 1, b = 1;
-	
-	for (int i=r+1; i<=n; i++) {
-		a *= i;
-		b *= (i-r);
-		if (a % (i-r) == 0) {
-			a /= (i-r);
-			b /= (i-r);
-		}
+	long long res = 1;
+	for (int i = 0; i < r; i++) {
+	res = res * (n - i) / (i + 1);  // 逐步相乘相除, 防止溢位
 	}
-	return a / b;
+	return res;
 }
 
 int main() {
 	int n, r;
 	cin >> n >> r;
 
-  // 數學上 C(n取r) r<0 與 r>n 根本沒意義, 在這邊搞人很有趣?
+	// 數學上 C(n取r) r<0 與 r>n 根本沒意義, 在這邊搞人很有趣?
 	if (r < 0 || r > n) {
 		cout << "0\n";
 		return 0;
-	} else if (n == r || r == 0) {
+	} else if (n == r || r == 0) { // 這樣比較快 <3
 		cout << "1\n";
 		return 0;
 	}
-	
+
 	cout << C(n, r) << "\n";
 	return 0;
 }
