592ecffbf208264fd33d75a51c2b37af50d86d20
Seqing <rushiatable@gmail.com>
2025-03-07 11:13:48 +0800

    Update sylvester (暫存).cpp

--- Kattis /sylvester (暫存).cpp
+++ Kattis /sylvester (暫存).cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int hadamard(int n, int x, int y) {
+// 記得 long long, debug 半天才發現 LOL
+int hadamard(long long n, long long x, long long y) {
 	if (n == 1) return 1;
-	int half = n/2;
+	long long half = n/2;
 	
 	if (x < half && y < half) return hadamard(half, x, y); // 左上
 	if (x >= half && y < half) return hadamard(half, x - half, y); // 右上
 	if (x < half && y >= half) return hadamard(half, x, y - half); // 左下
-	return -hadamard(half, x - half, y - half); // 右下 (取負)
+	return -hadamard(half, x - half, y - half); // 右下
 }
 
 int main() {
@@ -18,11 +19,12 @@ int main() {
 	int t; cin >> t;
 	
 	while (t--) {
-		int n, x, y, w, h;
+		long long n;
+		int x, y, w, h;
 		cin >> n >> x >> y >> w >> h;
 		
-		for (int i=y; i<y+h; i++) {
-			for (int j=x; j<x+w; j++) {
+		for (long long i=y; i<y+h; i++) {
+			for (long long j=x; j<x+w; j++) {
 				if (j == x) {
 					cout << hadamard(n, j, i);
 				} else {
