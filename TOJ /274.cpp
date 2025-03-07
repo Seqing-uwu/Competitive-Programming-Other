fbf7b9e43ae38539414a3367f9325aee144ae326
Seqing <rushiatable@gmail.com>
2025-03-07 11:53:50 +0800

    Create 274.cpp

---
+++ TOJ /274.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+string s;
+
+bool neko(int l, int r, bool removed) {
+	while (l < r) {
+		if (s[l] != s[r]) {
+			if (removed) return false;
+			
+			return neko(l+1, r, true) or neko(l, r-1, true);
+		}
+		l++; r--;
+	}
+	
+	return true;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n; cin >> n;
+    
+    while (n--) {
+    	cin >> s;
+    	
+    	if (neko(0, s.size() - 1, false)) cout << "YES\n";
+    	else cout << "NO\n";
+    }
+    
+    return 0;
+}
